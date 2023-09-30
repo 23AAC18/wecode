737727a3b369408e9d268ad6cca9f916e6a8e1d9
RevanthSallangula <theblazecode@gmail.com>
2023-09-30 20:33:48 +0530

    initialized fb

--- webpages/codingspace/codinglogic.js
+++ webpages/codingspace/codinglogic.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyANafOMY9kojKKxBa9hwKrXAH6u4uTXhcU",
+    authDomain: "wecode-91084.firebaseapp.com",
+    databaseURL:
+        "https://wecode-91084-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "wecode-91084",
+    storageBucket: "wecode-91084.appspot.com",
+    messagingSenderId: "107117565088",
+    appId: "1:107117565088:web:7c3d73d23bf094ecdca5c5",
+    measurementId: "G-S8SGHVTC2Z",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 document.addEventListener("DOMContentLoaded", () => {
     const socket = io();
 
