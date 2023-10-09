0edd9a578feb312234226553d425f76062366f32
shashankyadav17 <138793754+shashankyadav17@users.noreply.github.com>
2023-10-09 18:57:15 +0530

    react component of the pages

---
+++ webpages/codingspace/codingspace.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./coding.css";
+function CodingSpace()
+{
+    return(
+    <div>
+    <div className="header">
+        <div id="header-content">
+            <div id="header-text">WeCode</div>
+        </div>
+        <div className="search-group">
+            <input
+                type="text"
+                id="project-name-input"
+                name="project-name-input"
+                placeholder="search here"
+                required
+            />
+        </div>
+    </div>
+    <div id="sidebar">
+        <ul className="list-group">
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                Home
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                Search
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                New Project
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                Current Projects
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+               statistics
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                Foldres
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+                Active Coders
+            </li>
+            <li
+                className="list-group-item"
+                style="background-color: rgba(0, 0, 0, 0)"
+            >
+               Collabarations
+            </li>
+        </ul>
+    </div>
+    <div className="content">
+        <div className="button-container">
+            <button className="button" onClick="saveFunction()">Save</button>
+            <button className="button" onClick="runFunction()">Run</button>
+            <button className="button" onClick="debugFunction()">Debug</button>
+            <button className="button" onClick="stopFunction()">Stop</button>
+            <button className="button" onClick="shareFunction()">Share</button>
+        </div>
+        <textarea id="codingSpace" rows="4" cols="50"></textarea>
+    </div>
+    <script type="module" src="/webpages/codingspace/firebase.js"></script>
+</div>
+);
+}
+
+export default CodingSpace;
--- webpages/createproject/createproject.js
+++ webpages/createproject/createproject.js
@@ -1,4 +1,5 @@
 // import React from "react";
+// import "./page2.css";
 // function CreateProject() {
 //     return (
 //         <div>
