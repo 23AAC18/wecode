3a98337996a8c5d8d39270c3ad75748da0a58540
Revanth Sallangula <revanth.sallangula@icloud.com>
2023-10-31 17:06:34 +0530

    The Great Session Wars of 2023: Prologue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const http = require("http");
