d1f9978adc316448e96dc980f9c4273a8b528c50
RevanthSallangula <theblazecode@gmail.com>
2023-11-07 20:39:28 +0530

    Common sidebar.js for all pages added

--- webpages/createproject/sidebar.js
+++
@@ -1,9 +0,0 @@
-const createProjectButton = document.getElementById("create-project-button");
-const homeButton = document.getElementById("home-button");
-
-createProjectButton.addEventListener("click", function () {
-    window.location.replace("/createProject");
-});
-homeButton.addEventListener("click", function () {
-    window.location.replace("/home");
-});
