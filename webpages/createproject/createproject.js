449ea171dbfa5b9a1eb25d764b9078cfaa86addf
Revanth Sallangula <revanth.sallangula@icloud.com>
2023-11-08 18:39:04 +0530

    Connected createproject.js to Firebase

--- webpages/createproject/createproject.js
+++ webpages/createproject/createproject.js
@@ -1,4 +1,4 @@
-import { getCookie } from "/cookie.js";
+import { getCookie } from "../../cookie.js";
 
 const username = getCookie("username");
 
@@ -6,4 +6,24 @@ if (username) {
     console.log("Username from createproject.js:", username);
 } else {
     console.log("No username stored in the cookie");
+}
+
+// createProject.js
+
+import { db } from "../CurrentProjects/currentProjects.js";
+import {
+    ref,
+    set,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+const projectsRef = ref(db, "projects");
+
+function addProjectToDatabase(projectData) {
+    set(projectsRef, projectData)
+        .then(() => {
+            console.log("Project data added to the database");
+        })
+        .catch((error) => {
+            console.error("Error adding project data to the database:", error);
+        });
 }
