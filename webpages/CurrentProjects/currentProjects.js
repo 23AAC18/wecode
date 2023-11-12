787692214530d0a9d74e2bd6d0170aa0c8cae79d
RevanthSallangula <theblazecode@gmail.com>
2023-11-12 15:22:19 +0530

    Update currentProjects.js

--- webpages/CurrentProjects/currentProjects.js
+++ webpages/CurrentProjects/currentProjects.js
@@ -19,13 +19,7 @@ import {
 setInterval(checkUsernameChange, 10 * 60 * 1000); // Check username Change every 10 mins
 
 // Get username from cookie stored in browser
-const storedUsername = getCookie("username");
-
-if (storedUsername) {
-    console.log(storedUsername);
-} else {
-    //It honestly shouldn't come here, but if it does, you know who to call.
-}
+const username = getCookie("username");
 
 //Firebase Setup
 const firebaseConfig = {
