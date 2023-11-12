ec27b5a575413c3529e2948b37fef2cd414355e8
RevanthSallangula <theblazecode@gmail.com>
2023-11-12 15:24:47 +0530

    Update currentProjects.js

--- webpages/CurrentProjects/currentProjects.js
+++ webpages/CurrentProjects/currentProjects.js
@@ -37,4 +37,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase();
+document.addEventListener("DOMContentLoaded", async () => {
+    if (username) {
+        console.log(username);
+    } else {
+        console.error("No username stored in cookie");
+    }
+});
 export { db };
